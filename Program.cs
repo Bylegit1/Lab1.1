9bb9b67e04dbd9beede31bb5a6f45fc33cf40bbd
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-03 00:33:49 +0800

    Сделана декомпозиция

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
 ﻿namespace Lab1._1
 {
-    internal class Program
+    public class LuckyNumberLogic
+    {
+        public static bool IsNegative(int number)
+        {
+            return number < 0;
+        }
+
+        public static bool IsSixDigit(int number)
+        {
+            return number >= 100000 && number <= 999999;
+        }
+
+        public static string CheckLuckyNumber(int number)
+        {
+            var digit1 = number / 100000;
+            var digit2 = (number / 10000) % 10;
+            var digit3 = (number / 1000) % 10;
+            var digit4 = (number / 100) % 10;
+            var digit5 = (number / 10) % 10;
+            var digit6 = number % 10;
+
+            var firstSum = digit1 + digit2 + digit3;
+            var secondSum = digit4 + digit5 + digit6;
+
+            if (firstSum == secondSum)
+            {
+                return "Число является счастливым";
+            }
+            else
+            {
+                return "Число не является счастливым";
+            }
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
@@ -8,34 +43,17 @@
             var number = int.Parse(Console.ReadLine());
 
             var result = "";
-            if (number < 0)
+            if (LuckyNumberLogic.IsNegative(number))
             {
                 result = "Число является отрицательным";
             }
-            else if (number < 100000 || number > 999999)
+            else if (!LuckyNumberLogic.IsSixDigit(number))
             {
                 result = "Число не является шестизначным";
             }
             else
             {
-                var digit1 = number / 100000;
-                var digit2 = (number / 10000) % 10;
-                var digit3 = (number / 1000) % 10;
-                var digit4 = (number / 100) % 10;
-                var digit5 = (number / 10) % 10;
-                var digit6 = number % 10;
-
-                var firstSum = digit1 + digit2 + digit3;
-                var secondSum = digit4 + digit5 + digit6;
-
-                if (firstSum == secondSum)
-                {
-                    result = "Число является счастливым";
-                }
-                else
-                {
-                    result = "Число не является счастливым";
-                }
+               result = LuckyNumberLogic.CheckLuckyNumber(number);
             }
 
             Console.WriteLine(result);
