bb5c0bdc63741bd911e057ebc218523b0f99aec2
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-02 23:55:48 +0800

    Ввод 6-ого числа и разбиение его на цифры

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите шестизначное число: ");
+            var number = int.Parse(Console.ReadLine());
+
+            if(number < 100000 || number > 999999)
+            {
+                Console.WriteLine("Число не является шестизначным");
+            }
+            else
+            {
+                var digit1 = number / 100000;
+                var digit2 = (number / 10000) % 10;
+                var digit3 = (number / 1000) % 10;
+                var digit4 = (number / 100) % 10;
+                var digit5 = (number / 10) % 10;
+                var digit6 = number % 10;
+            }
         }
     }
 }
