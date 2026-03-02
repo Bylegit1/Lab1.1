239ace98a24dca77708aa07a4cb318e03707ab3b
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-03 00:06:03 +0800

    Задача решена, осталось убрать лапшу и сделать декомпозицию

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@
         {
             Console.WriteLine("Введите шестизначное число: ");
             var number = int.Parse(Console.ReadLine());
-
-            if(number < 100000 || number > 999999)
+            if (number < 0)
+            {
+                Console.WriteLine("Число является отрицательным");
+            }
+            else if (number < 100000 || number > 999999)
             {
                 Console.WriteLine("Число не является шестизначным");
             }
@@ -19,6 +22,18 @@
                 var digit4 = (number / 100) % 10;
                 var digit5 = (number / 10) % 10;
                 var digit6 = number % 10;
+
+                var firstSum = digit1 + digit2 + digit3;
+                var secondSum = digit4 + digit5 + digit6;
+
+                if (firstSum == secondSum)
+                {
+                    Console.WriteLine("Число является счастливым");
+                }
+                else
+                {
+                    Console.WriteLine("Число не является счастливым");
+                }
             }
         }
     }
